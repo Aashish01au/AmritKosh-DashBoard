f86f6ee8fbd9075e3e2ca24bbc858fe9fd994457
Aashish Sah <aashish01au@gmail.com>
2024-05-16 15:38:13 +0545

    modifying

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,42 +1,9 @@
-"use client";
+import React from 'react'
 
-import Link from "next/link";
-
-interface ProductProps {
-  _id: string;
-  userId: string;
-  babyName: string;
-  dateOfBaby: string;
-  engDateOfBaby: string;
-  gestationalAge: number;
-  ipNumber: number;
-  babyWeight: number;
-  diagnosis: string;
-  indications: string;
-  babyStatus: string;
-  milkConsumed: number;
-  milkConsumedToday: number;
-  createdAt: string;
-  updatedAt: string;
-}
-
-interface props {
-  products: ProductProps[];
-}
-
-const Products = ({ products }: props) => {
-  
+const Products = () => {
   return (
-    <>
-      {products?.map((item) => (
-        <Link href={"/"} key={item._id}>
-          <div>
-            <h1>{item?.babyName}</h1>
-          </div>
-        </Link>
-      ))}
-    </>
-  );
-};
+    <div>Products</div>
+  )
+}
 
-export default Products;
+export default Products
